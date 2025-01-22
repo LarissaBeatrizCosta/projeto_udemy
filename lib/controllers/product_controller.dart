63a000ef06754f0015763fb54a6f1a223e70c8b1
v3x <larissa.beatrizzcosta@gmail.com>
2025-01-22 17:08:24 -0300

    Banco funcionou

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,14 +1,14 @@
-import 'package:get_storage/get_storage.dart';
-import 'package:udemy_curso_app/models/products_model.dart';
-
-// Future<void> saveProduct(List<ProductsModel> products) async {
+// import 'package:get_storage/get_storage.dart';
+// import 'package:udemy_curso_app/models/products_model.dart';
+//
+// // Future<void> saveProduct(List<ProductsModel> products) async {
+// //   GetStorage box = GetStorage();
+// //   box.write('products', value);
+// //   await box.save();
+// // }
+//
+// Future<String?> loadProduct() async {
 //   GetStorage box = GetStorage();
-//   box.write('products', value);
-//   await box.save();
+//   final String? value = box.read('userToken');
+//
 // }
-
-Future<String?> loadProduct() async {
-  GetStorage box = GetStorage();
-  final String? value = box.read('userToken');
-
-}
